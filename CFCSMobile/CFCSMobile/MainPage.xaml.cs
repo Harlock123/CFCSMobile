9926d03ecf4d437bc4100467643704747ca3fea7
harlo <harlo@WINDEV1612SYS>
2018-09-18 13:36:12 -0400

    Setup A SHA256Test

--- CFCSMobile/CFCSMobile/MainPage.xaml.cs
+++ CFCSMobile/CFCSMobile/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 using Xamarin.Forms;
 
 namespace CFCSMobile
@@ -12,6 +13,23 @@ namespace CFCSMobile
         public MainPage()
         {
             InitializeComponent();
+
+            
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            byte[] hash;
+
+            SHA256Managed alg = new SHA256Managed();
+
+            hash = alg.ComputeHash(System.Text.Encoding.UTF8.GetBytes(txtEntry.Text));
+
+            lblOUT.Text = Convert.ToBase64String(hash);
+
+
+
         }
     }
 }
