7231904f548e4a8048aa4be47c74791c1c040ba9
Lonnie <harlock123@gmail.com>
2019-02-06 08:20:51 -0500

    Updated packaging and setup for MSI Bigboy dev env

--- CFCSMobile/CFCSMobile/Settings.cs
+++ CFCSMobile/CFCSMobile/Settings.cs
@@ -8,10 +8,12 @@ namespace CFCSMobile
     public static class Settings
     {
 
-        static public string BASEURL = "http://192.168.225.128/CFCSMobileWebServices/api"; // VM running in Work Mac
+        //static public string BASEURL = "http://192.168.225.128/CFCSMobileWebServices/api"; // VM running in Work Mac
+
+        static public string BASEURL = "http://192.168.12.55/CFCSMobileWebServices/api"; // MSI Big Boy 
 
         //static public string BASEURL = "http://192.168.193.128/CFCSMobileWebServices/api"; // VM running in Linux Host Machine
-        
+
         //static public string BASEURL = "http://192.168.1.250/CFCSMobileWebServices/api"; // Machine At Home
 
 
